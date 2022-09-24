82d7186a7cc2743913d9c0bfe18be66a0a9797ab
Víctor Castillo~ <victorcg_castilloglzisc@hotmail.com>
2022-09-23 21:01:42 -0500

    Se configuró voista para enviar datos a controlador, ya actualiza información en bd y retorna vitsta con datos actualizados

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,2 +1,7 @@
+ <form action=" {{ url('/empleado/' . $empleado->id) }} " method="post" enctype="multipart/form-data">
+    @csrf
+    {{ method_field('PATCH') }}
+    
+    @include('empleado.form')
+</form>
  
- @include('empleado.form')
