c4a9240303a6500d1bf697b618db4741bcc9a348
Víctor Castillo~ <victorcg_castilloglzisc@hotmail.com>
2022-09-26 21:56:31 -0500

    Se corrigió problema en parcial de formulario generico de create y edit, al momento de la recepción del objeto en lla ruta. Ya crea y edita sin problema

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,18 +1,20 @@
 <label for="Nombre"> Nombre </label>
-<input type="text" name="Nombre" value=" {{$empleado->Nombre}} " id="Nombre"> <br>
+<input type="text" name="Nombre" value="{{ isset($empleado->Nombre) ? $empleado->Nombre : '' }}" id="Nombre"> <br>
 
 <label for="ApellidoPaterno"> Apellido Paterno </label>
-<input type="text" name="ApellidoPaterno" value=" {{$empleado->ApellidoPaterno}} " id="ApellidoPaterno"> <br>
+<input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno) ? $empleado->ApellidoPaterno : '' }}" id="ApellidoPaterno"> <br>
 
 <label for="ApellidoMaterno"> Apellido Materno </label>
-<input type="text" name="ApellidoMaterno" value=" {{$empleado->ApellidoMaterno}} " id="ApellidoMaterno" > <br>
+<input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno) ? $empleado->ApellidoMaterno : '' }}" id="ApellidoMaterno" > <br>
 
 <label for="Correo"> Correo </label>
-<input type="text" name="Correo" value=" {{$empleado->Correo}} " id="Correo" > <br>
+<input type="text" name="Correo" value="{{ isset($empleado->Correo) ? $empleado->Correo : '' }}" id="Correo" > <br>
 
 <label for="Foto"> Foto: </label>
 
-<img src=" {{ asset('storage'). '/' . $empleado->Foto }} " alt="" srcset="">
+@if(isset($empleado->Foto))
+    <img src=" {{ asset('storage'). '/' . $empleado->Foto }} " alt="" srcset="">
+@endif
 
 <input type="file" name="Foto" id="Foto" > <br>
 
